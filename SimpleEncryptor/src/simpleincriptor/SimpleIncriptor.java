d717d7029411096d2b3fd97bc5be70ee296ce925
Vitalii Besporstov <vitaliibesporstov@gmail.com>
2018-10-14 16:18:39 -0700

    Simple Encryptor

---
+++ SimpleEncryptor/src/simpleincriptor/SimpleIncriptor.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleincriptor;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author root
+ */
+public class SimpleIncriptor {
+
+    char chValv=(char)255;
+    private static String flipBits(String strData)
+    {
+        String strResult ="";
+        
+        for (int nIndex =0; nIndex< strData.length(); ++nIndex)
+        {
+           strResult+= (char)~(strData.charAt(nIndex));
+            
+        }
+        return strResult;
+    }
+    
+    
+    private static String simplyEncript(String strData)
+    {
+    long lStartTime =System.nanoTime();
+    String strValue = flipBits(strData);
+    long lDuration = System.nanoTime()-lStartTime;
+    return strValue;
+    }
+    
+    private static String simplyDecrypt(String strData)
+    {
+    long lStartTime =System.nanoTime();
+    String strValue = flipBits(strData);
+    long lDuration = System.nanoTime()-lStartTime;
+    return strValue;
+    }
+    
+    public static void main(String[] args) {
+        String strValue = JOptionPane.showInputDialog(null, "input the string");
+        String strEncrypted = simplyEncript(strValue);
+        JOptionPane.showMessageDialog(null, strEncrypted);
+        String strDecrypted = simplyDecrypt(strEncrypted);
+        JOptionPane.showMessageDialog(null, strDecrypted);
+    }
+    
+    
+
+    
+}
